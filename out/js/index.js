0b170d46f8e0722720ab65933145e81c4ad12321
kujarung <ryan@bliex.com>
2020-09-22 11:25:11 +0900

    test5

--- out/js/index.js
+++ out/js/index.js
@@ -1,4 +1,7 @@
-
+window.addEventListener('resize', () => {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
 
 window.onload = () => {
   let vh = window.innerHeight * 0.01;
