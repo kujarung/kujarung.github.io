1064ce196f949d0f71290900002307e6e035ee9d
kujarung <ryan@bliex.com>
2020-10-27 18:57:40 +0900

    Init out3

---
+++ out3/assets/js/index.js
@@ -0,0 +1,25 @@
+cssVars({
+  // Only styles from CodePen's CSS panel
+  include: 'style:not([data-ignore])',
+  // Treat all browsers as legacy
+  onlyLegacy: false,
+
+  // DEMO: Toggles to see results
+  // ----------------------------
+  // preserveStatic: false,
+  // preserveVars: true,
+  // updateURLs: false,
+  // variables: { '--color': 'purple' },
+  // ----------------------------
+
+  // Display transformed CSS
+  onComplete: function (cssText, styleNodes, cssVariables, benchmark) {
+    var codeElm = document.querySelector('code');
+
+    // Format CSS (external library)
+    cssText = css_beautify(cssText);
+
+    // Update <code> tag with CSS result
+    codeElm.textContent = cssText;
+  },
+});
