93e72ccfa0248a63765920f383034775da561574
kuja2314 <rnwkfydwkd82@likelion.org>
2020-11-22 22:47:31 +0900

    fix 추가 수정 사항 수정

--- out3/assets/js/index.js
+++ out3/assets/js/index.js
@@ -1,19 +1,42 @@
-cssVars({
-  // Only styles from CodePen's CSS panel
-  include: 'style:not([data-ignore])',
-  // Treat all browsers as legacy
-  onlyLegacy: false,
+var selectItem = 'KOR';
+var array = ['ENG', 'KOR', 'SPAN'];
+var orderItem = function () {
+  orderArray(selectItem);
+  array.forEach(function (item) {
+    var isActive = item === selectItem ? ' active' : '';
+    var appendItem =
+      '<li class="' +
+      item +
+      isActive +
+      '" onclick=  clickEvent(' +
+      "'" +
+      item +
+      "'" +
+      ') >' +
+      item +
+      '</li>';
+    $('.language-menu').append(appendItem);
+  });
+};
 
-  // DEMO: Toggles to see results
-  // ----------------------------
-  // preserveStatic: false,
-  // preserveVars: true,
-  // updateURLs: false,
-  // variables: { '--color': 'purple' },
-  // ----------------------------
+var orderArray = function (selectItem) {
+  var findIndex = array.indexOf(selectItem);
+  array.splice(findIndex, 1);
+  array.unshift(selectItem);
+};
 
-  // Display transformed CSS
-  onComplete: function (cssText, styleNodes, cssVariables, benchmark) {
-    var codeElm = document.querySelector('code');
-  },
-});
+var targetClickEvent = function () {
+  if ($(this).hasClass('active')) {
+    $(this).removeClass('active');
+  } else {
+    $(this).addClass('active');
+  }
+};
+
+var clickEvent = function (item) {
+  $('.language-menu' + ' li').removeClass('active');
+  $(item).addClass('active');
+  selectItem = item;
+  $('.language-menu').children().remove();
+  this.orderItem(selectItem);
+};
